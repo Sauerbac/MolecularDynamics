d6e699ce35bd263f6bd0d9233b043a911df71d4b
Sauerbach <s1auerbach@aol.com>
2022-09-16 09:20:59 +0200

    ms7 rumprobieren

--- milestones/07/main.cpp
+++ milestones/07/main.cpp
@@ -10,101 +10,89 @@
 #include <Eigen/Dense>
 #include <iostream>
 
+double avg_temp(double avg_kin, int nb_atoms) {
+    return avg_kin / ((3.0 / 2.0) * BOLTZMANN * nb_atoms);
+}
+
+void write_avgs(std::ofstream &file, double avg_temp, double potential_energy) {
+
+    file << avg_temp << std::setw(15) << potential_energy << std::endl;
+}
+
 int main() {
     std::cout << "Hello milestone 07!" << std::endl;
 
-    // std::string path =
-    //     "/home/sauerbach/HPC/MolecularDynamics/static/cluster_923.xyz";
-    // Atoms system = read_atoms_no_velocities(path);
-    // // Atoms system = cubic(300, 3.0, 196.966569, 0.01);
-    // std::cout << system.nb_atoms() << std::endl;
+    std::string path =
+        "/home/sauerbach/HPC/MolecularDynamics/static/cluster_309.xyz";
+    Atoms system = read_atoms_no_velocities(path);
+    // Atoms system = cubic(300, 3.0, 196.966569, 0.01);
+    std::cout << system.nb_atoms() << std::endl;
 
     // std::string xyz_out =
     //     "/home/sauerbach/HPC/MolecularDynamics/static/out_no_domain.xyz";
     // std::ofstream file(xyz_out);
 
     // std::string metrics =
-    //     "/home/sauerbach/HPC/MolecularDynamics/static/metrics.csv";
+    //     "/home/sauerbach/HPC/MolecularDynamics/static/metrics_309.csv";
     // std::ofstream metrics_file(metrics);
 
-    // NeighborList neighbor_list(7.0);
+    std::string avg_temp_path =
+        "/home/sauerbach/HPC/MolecularDynamics/static/temps_309.csv";
+    std::ofstream temp_file(avg_temp_path);
 
-    // double timestep = 10;
-    // double goal_temp = 500;
-    // double relaxation_time = 10000;
+    NeighborList neighbor_list(5.0);
 
-    // int num_steps = relaxation_time / timestep;
+    double timestep = 1;
+    double period_time = 10000;
+    int period_steps = period_time / timestep;
+    int tau = 100;
 
-    // for (int j = 0; j < 50; j++) {
-    //     for (int i = 0; i < num_steps; i++) {
-    //         if (i % 10 == 0) {
-    //             write_xyz(file, system);
-    //         }
+    double temp = 460;
 
-    //         neighbor_list.update(system);
-    //         verlet1(system, timestep);
-    //         double pot = gupta(system, neighbor_list);
-    //         verlet2(system, timestep);
-    //         berendsen_thermostat(system, goal_temp, timestep,
-    //         relaxation_time);
-    //         // std::cout << temperature(system) << std::endl;
-    //         write_metrics(metrics_file, system, pot);
-    //     }
-    //     goal_temp = goal_temp + 10;
-    //     for (int i = 0; i < num_steps * 2; i++) {
-    //         if (i % 10 == 0) {
-    //             write_xyz(file, system);
-    //         }
+    for (int i = 0; i < period_steps; i++) {
 
-    //         neighbor_list.update(system);
-    //         verlet1(system, timestep);
-    //         double pot = gupta(system, neighbor_list);
-    //         verlet2(system, timestep);
-    //         // std::cout << temperature(system) << std::endl;
-    //         write_metrics(metrics_file, system, pot);
-    //     }
-    //     std::cout << j << std::endl;
-    //     std::cout << goal_temp << ' ' << temperature(system) << std::endl;
-    // }
-
-    // file.close();
-
-    std::string path =
-        "/home/sauerbach/HPC/MolecularDynamics/static/cluster_923.xyz";
-    std::string xyz_out =
-        "/home/sauerbach/HPC/MolecularDynamics/static/out_no_domain.xyz";
-    std::ofstream file(xyz_out);
-    std::string metrics =
-        "/home/sauerbach/HPC/MolecularDynamics/static/metrics.csv";
-    std::ofstream metrics_file(metrics);
-
-    Atoms system = read_atoms_no_velocities(path);
-    double cutoff_distance = 10.0;
-    NeighborList neighbor_list(cutoff_distance);
-
-    double timestep = 10;
-    double goal_temp = 1000;
-    double relaxation_time = 10000;
-
-    int num_steps = relaxation_time / timestep;
-    for (int i = 0; i < num_steps; i++) {
-
-        verlet1(system, timestep);
-
-        double pot = 0.0;
         neighbor_list.update(system);
-
-        pot = gupta(system, neighbor_list);
+        verlet1(system, timestep);
+        double pot = gupta(system, neighbor_list);
         verlet2(system, timestep);
-        // berendsen_thermostat(system, goal_temp, timestep,
-        //                      relaxation_time / 100);
-        if (i % (num_steps / 100) == 0) {
-
-            // std::cout << system.nb_atoms() << std::endl;
-            write_xyz(file, system);
-            write_metrics(metrics_file, system, pot);
-        }
+        berendsen_thermostat(system, temp, timestep, tau);
     }
 
-    return 0;
+    for (int j = 0; j < 100; j++) {
+
+        vec kinetic_energys(period_steps);
+        double pot = 0;
+
+        for (int i = 0; i < period_steps; i++) {
+
+            neighbor_list.update(system);
+            verlet1(system, timestep);
+            pot = gupta(system, neighbor_list);
+            verlet2(system, timestep);
+
+            kinetic_energys.row(i) = kinetic_energy(system);
+        }
+
+        double a_temp = avg_temp(kinetic_energys.mean(), system.nb_atoms());
+        write_avgs(temp_file, a_temp, pot);
+        std::cout << "Sclaing" << std::endl;
+        double factor = sqrt(1 / kinetic_energys.mean() + 1);
+        system.velocities *= factor;
+
+        for (int i = 0; i < period_steps; i++) {
+
+            neighbor_list.update(system);
+            verlet1(system, timestep);
+            pot = gupta(system, neighbor_list);
+            verlet2(system, timestep);
+
+            // write_metrics(metrics_file, system, pot);
+            // if (i % 100 == 0) {
+            //     write_xyz(file, system);
+            // }
+        }
+    }
+    std::cout << period_steps << std::endl;
+
+    // file.close();
 }
